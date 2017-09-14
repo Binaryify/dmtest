faaf51b7410f1e54113a65b06d70cce7972aadef
binaryify <zhuangtongfa@qq.com>
2017-09-14 11:35:17 +0800

    test

---
+++ docs/HTML5 canvas和mp4视频真实交互弹幕 » 张鑫旭-鑫空间-鑫生活_files/ca-pub-0090627341039040.js
@@ -0,0 +1,1 @@
+try{window.localStorage.setItem('google_pub_config','{"sraConfigs":{"2":{"sraEnabled":true,"sraTimeout":60000,"whitelist":{"14dm4lv,2":1,"3k8lir,2":1,"1o1t4ek,2":1}},"4":{"sraEnabled":true,"sraTimeout":60000,"whitelist":{"14dm4lv,2":1,"1o1t4ek,2":1,"3k8lir,2":1}}}}');}catch(e){}
